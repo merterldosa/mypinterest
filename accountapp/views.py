2a701a0595564f56cd8113ed2a2bc1b13321ae18
merterl <merterldosa@gmail.com>
2023-08-02 10:20:42 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -13,6 +15,6 @@ def hello_world(request):
         new_hello_world.text = get
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'new_hello_world' : new_hello_world.text})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET REQUEST'})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': HelloWorld.objects.all()})
