f2a4491bfcac8b1204f87010229fed151bec1ac5
merterl <merterldosa@gmail.com>
2023-08-04 14:37:52 +0900

    django course 36 commit

---
+++ articleapp/forms.py
@@ -0,0 +1,9 @@
+# articleapp/forms.py 작성
+from django.forms import ModelForm
+from articleapp.models import Article
+
+
+class ArticleCreationForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image', 'content'] # writer 는 서버 내에서 설정해줄 것이므로 제외
