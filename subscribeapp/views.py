e50dbe8dd73ba0216296e909eced299132b0c1e7
merterl <merterldosa@gmail.com>
2023-08-06 14:05:29 +0900

    django course 43 commit

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -36,6 +37,16 @@ class SubscriptionView(RedirectView):
             Subscription(user=user, project=project).save() # -> 앞서 찾은 user,project 로 구독정보를 만들어야함.
             # 대문자 S
 
-
-
         return super(SubscriptionView, self).get(request, *args, **kwargs)
+
+@method_decorator(login_required, 'get') # 로그인 하였는지
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 5
+
+    def get_queryset(self): # 가지고 오는 게시글들의 조건을 바꿀 수 있는 함수
+        projects = Subscription.objects.filter(user=self.request.user).values_list("project") #values_list : 값들을 리스트화 시킨다는 의미.
+        article_list = Article.objects.filter(project__in=projects) # 바로 위에서 find 한 projects
+        return article_list
