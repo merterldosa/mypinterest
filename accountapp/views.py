b0e9ccc61ffdf9e74135fc63af82c1b12d2fe138
merterl <merterldosa@gmail.com>
2023-08-08 19:03:01 +0900

    docker image make fail 두번째- modify

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
-from django.shortcuts import render
-
-# Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic.list import MultipleObjectMixin
-
-from accountapp.models import HelloWorld
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from accountapp.forms import AccountUpdateForm
